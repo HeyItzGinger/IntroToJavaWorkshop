0a2daa5c2d353b849d26353edbc7fb950e8b7adb
Natalie Petersen <league@WTS13.attlocal.net>
2016-09-21 16:57:50 -0700

    Day 3

---
+++ src/day3/Readers.java
@@ -0,0 +1,11 @@
+package day3;
+
+import javax.print.attribute.standard.JobKOctetsProcessed;
+import javax.swing.JOptionPane;
+
+public class Readers {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "hello");
+	System.out.println("Im Awesome");
+}
+}
